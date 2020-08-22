68925ce093ca3b368145a2d5081499f3eb608a06
Jeff <me@jeffbenson.io>
2020-08-22 17:37:33 -0400

    added back translation and diagram

--- testing.py
+++ testing.py
@@ -4,11 +4,20 @@ import sys
 def main():
     print('Testing!')
     print('liblouis version: ' + louis.version())
+    print('Table Check passed!')
+    louis.checkTable([b'./en-ueb-g2.ctb'])
     # something = input('type something: ')
     something = sys.stdin.readline().strip()
     print("Here's what you typed: " + something)
-    braille = louis.translateString([b'./en-ueb-g2.ctb'],something, mode=4)
+    # prints the value of something and combines the ucBrl (64) and noUndefined (128) modes. Outputs as a single word, so a list is necessary to grab individual characters
+    braille = louis.translateString([b'./en-ueb-g2.ctb'],something,mode=192)
     print(list(braille))
+    print("Here it is in dots:")
+    dots = louis.charToDots([b'./en-ueb-g2.ctb'],something,mode=192)
+    print("Here's a back-translation of what you typed:")
+    # Returns typed phrases without contractions, for proper bash interpretation
+    backBraille = louis.backTranslateString([b'./en-ueb-g2.ctb'],braille,mode=1)
+    print(backBraille)
 
 if __name__ == "__main__":
     main()
